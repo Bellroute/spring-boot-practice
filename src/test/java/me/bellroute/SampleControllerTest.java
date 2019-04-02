fa5e1ab300fa04826cd83ea107aca7832bdee227
Bellroute <kjkun7631@gmail.com>
2019-04-02 22:30:56 +0900

    do it: ~ 스프링 웹 MVC 8부: HtmlUnit

---
+++ src/test/java/me/bellroute/SampleControllerTest.java
@@ -0,0 +1,55 @@
+package me.bellroute;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
+public class SampleControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebClient webClient;
+
+    @Test
+    public void hello() throws Exception {
+        // 요청 "/"
+        // 응답
+        // - 모델 name : bellroute
+        // - 뷰 이름: hello
+
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("name", is("bellroute")))
+                .andExpect(content().string(containsString("bellroute")));
+    }
+
+    @Test
+    public void hello2() throws Exception {
+        // html에 특화된 테스트
+
+        HtmlPage page = webClient.getPage("/hello");
+        HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+        assertThat(h1.getTextContent()).isEqualToIgnoringCase("bellroute");
+    }
+}
